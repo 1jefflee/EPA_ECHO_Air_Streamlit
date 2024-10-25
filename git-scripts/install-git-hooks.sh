cbeb44df34f5609b598993bfb0b7ecf6f870c45a
1jefflee <1jefflee@gmail.com>
2024-10-25 10:16:41 -0400

    updates

--- git-scripts/install-git-hooks.sh
+++ git-scripts/install-git-hooks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Installing Git hooks..."
 
+# Navigate to the root of the Git repository
+cd "$(git rev-parse --show-toplevel)" || exit
+
 # Copy the pre-push hook to the .git/hooks directory
 cp git-scripts/git-hooks/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
